4dd31c502028afe2af6869f47934d1ecc5b2e576
B.C <123095997+Inval1dUser@users.noreply.github.com>
2023-02-08 20:58:29 -0500

    Add files via upload

---
+++ Cheat Sheets/LoopsCheatSheet.cpp
@@ -0,0 +1,27 @@
+/*
+Cheat Sheet: Loops
+Date: 2/8/2023
+*/
+
+
+//For loop
+//A for loop is a type of loop that allows you to execute a set of instructions multiple times. It is typically used when you know how many times the loop should run. 
+//Syntax: 
+for (initialization; condition; increment/decrement) { 
+   statement(s); 
+} 
+  
+//While loop
+//A while loop is a type of loop that allows you to execute a set of instructions multiple times as long as the condition remains true. It is typically used when you don’t know how many times the loop should run. 
+//Syntax: 
+while (condition) { 
+   statement(s); 
+} 
+
+  
+//Do-while loop
+//A do-while loop is similar to a while loop, except that it will always execute at least once before checking the condition. It is typically used when you want to make sure that the code inside the loop runs at least once, regardless of the condition. 
+//Syntax: 
+do { 
+   statement(s); 
+} while (condition);
